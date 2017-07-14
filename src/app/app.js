11282d88cf76623296c420e24507646119f520ff
jcambert <jc.ambert@free.fr>
2017-07-14 21:14:40 +0200

    Start configuring

--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,46 @@
 'use strict';
 
-angular.module('BlurAdmin', [
-  'ngAnimate',
-  'ui.bootstrap',
-  'ui.sortable',
-  'ui.router',
-  'ngTouch',
-  'toastr',
-  'smart-table',
-  "xeditable",
-  'ui.slimscroll',
-  'ngJsTree',
-  'angular-progress-button-styles',
+angular
+    .module('BlurAdmin', [
+        'ngAnimate',
+        'ui.bootstrap',
+        'ui.sortable',
+        'ui.router',
+        'ngTouch',
+        'toastr',
+        'smart-table',
+        "xeditable",
+        'ui.slimscroll',
+        'ngJsTree',
+        'angular-progress-button-styles',
 
-  'BlurAdmin.theme',
-  'BlurAdmin.pages'
-]);
+        'BlurAdmin.theme',
+        'BlurAdmin.pages'
+    ])
+
+
+.constant('appConfig', {
+    title: '<span>ATF</span>&nbsp;Board',
+    logo: '',
+    state: 'dashboard'
+
+})
+
+.controller('mainCtrl', MainController)
+
+.filter('trust', trustHtml)
+
+;
+/** @ngInject */
+function MainController($scope, appConfig) {
+    $scope.config = appConfig;
+
+}
+
+/** @ngInject */
+function trustHtml($sce) {
+    return function(html) {
+        return $sce.trustAsHtml(html);
+    }
+
+}
